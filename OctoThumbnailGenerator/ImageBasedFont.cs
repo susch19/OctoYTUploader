8033f1f4be2ee108da449fb3fc842ae1ca3ab4d1
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-07-01 18:54:30 +0200

    Upgrade to .net 5.0
    Try to fix all issues and upgrade to .net 5.0

---
+++ OctoThumbnailGenerator/ImageBasedFont.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoThumbnailGenerator
+{
+    class ImageBasedFont
+    {
+        private readonly Dictionary<char, FileInfo> imagePaths;
+
+        public ImageBasedFont(IEnumerable<(char Key, FileInfo Path)> images)
+        {
+            imagePaths = images.ToDictionary(c => c.Key, c => c.Path);
+        }
+
+        public Bitmap GetFont(char value, int width, int height)
+        {
+            if (imagePaths.TryGetValue(value, out var path))
+            {
+                using (var original = Image.FromFile(path.FullName))
+                {
+                    var bitmap = new Bitmap(width, height);
+                    var graphic = Graphics.FromImage(bitmap);
+                    graphic.DrawImage(original, 0,0, width, height);
+                    return bitmap;
+                }
+            };
+
+            return null;
+        }
+
+        public void DrawFont(Graphics graphics, Point position, string text, int margin, Func<Size, Size> rescale = null)
+        {
+            var lastPos = position.X;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (imagePaths.TryGetValue(text[i], out var path))
+                {
+                    using (var original = Image.FromFile(path.FullName))
+                    {
+                        var usedSize = rescale == null ? original.Size : rescale(original.Size);
+                        graphics.DrawImage(original, new Rectangle(new Point(lastPos, position.Y), usedSize));
+                        lastPos += usedSize.Width + margin;
+                    }
+                }
+            }
+        }
+
+    }
+}
--- OctoThumbnailGenerator/MainProgram.cs
+++ OctoThumbnailGenerator/MainProgram.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
@@ -14,33 +15,31 @@ namespace OctoThumbnailGenerator
             var fromNumberInclusive = int.Parse(Console.ReadLine());
             Console.Write("Folgennummer bis: ");
             var toNumberInclusive = int.Parse(Console.ReadLine());
-            Image bitmap = Image.FromFile("octoawesome.png");
+            var bitmap = Image.FromFile("octoawesome.png");
             if (!Directory.Exists("Fertige Thumbnails"))
                 Directory.CreateDirectory("Fertige Thumbnails");
             Directory.SetCurrentDirectory("Fertige Thumbnails");
+
+            var file = new FileInfo(@"D:\LP's\Stream\OctoAwesome\PNGFont\awesome.fdef");
+            var folder = file.Directory;
+            var defFile = JsonConvert.DeserializeObject<ImageFontDefiniton>(File.ReadAllText(file.FullName));
+
+            var font = defFile.Mapping.Select(a => (a.Key[0], new FileInfo(Path.Combine(folder.FullName, a.Value))));
+            var imageFont = new ImageBasedFont(font);
+
             Image newBmp;
-            Brush b = new SolidBrush(Color.FromArgb(255, 254, 0, 0));
-            Pen pen = new Pen(Color.Black, 10);
-            Console.ForegroundColor = ConsoleColor.White;
-            var fam = FontFamily.Families.FirstOrDefault(f => f.Name == "Calibri");
-            for (; fromNumberInclusive <= toNumberInclusive; fromNumberInclusive++)
+            using (var b = new SolidBrush(Color.FromArgb(255, 254, 0, 0)))
             {
-                newBmp = (Image)bitmap.Clone();
-                using (Graphics g = Graphics.FromImage(newBmp))
+                for (; fromNumberInclusive <= toNumberInclusive; fromNumberInclusive++)
                 {
-                    GraphicsPath p = new GraphicsPath();
-                    p.AddString(
-                        "#" + fromNumberInclusive,
-                        fam,
-                        (int)FontStyle.Bold,
-                        g.DpiY * 275 / 90,
-                        new Point(540, 300),
-                        new StringFormat());
-                    g.DrawPath(pen, p);
-                    g.FillPath(b, p);
+                    newBmp = (Image)bitmap.Clone();
+                    using (var g = Graphics.FromImage(newBmp))
+                    {
+                        imageFont.DrawFont(g, new Point(955,24), "#" + fromNumberInclusive, 0, rescale: size => RescaleCubic(size, height: size.Height - 194));
+                    }
+                    newBmp.Save($"octoawesome{fromNumberInclusive}.png");
+                    Console.WriteLine($"octoawesome{fromNumberInclusive}.png saved");
                 }
-                newBmp.Save($"octoawesome{fromNumberInclusive}.png");
-                Console.WriteLine($"octoawesome{fromNumberInclusive}.png saved");
             }
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Finished. Press any key to close :)");
@@ -49,24 +48,43 @@ namespace OctoThumbnailGenerator
 
         public static Image GenerateThumbnail(int version)
         {
-            Image newBmp = Image.FromFile("octoawesome.png");
-            Brush b = new SolidBrush(Color.FromArgb(255, 254, 0, 0));
-            Pen pen = new Pen(Color.Black, 10);
-            using (Graphics g = Graphics.FromImage(newBmp))
+            var newBmp = Image.FromFile("octoawesome.png");
+            using (Brush b = new SolidBrush(Color.FromArgb(255, 254, 0, 0)))
             {
-                GraphicsPath p = new GraphicsPath();
-                p.AddString(
-                    "#" + version,
-                    FontFamily.Families.FirstOrDefault(f => f.Name == "Calibri"),
-                    (int)FontStyle.Bold,
-                    g.DpiY * 275 / 90,
-                    new Point(540, 300),
-                    new StringFormat());
-                g.DrawPath(pen, p);
-                g.FillPath(b, p);
-                return newBmp;
+                using (var pen = new Pen(Color.Black, 10))
+                {
+                    using (var g = Graphics.FromImage(newBmp))
+                    {
+                        using (var p = new GraphicsPath())
+                        {
+                            p.AddString(
+                                "#" + version,
+                                FontFamily.Families.FirstOrDefault(f => f.Name == "Calibri"),
+                                (int)FontStyle.Bold,
+                                g.DpiY * 275 / 90,
+                                new Point(540, 300),
+                                new StringFormat());
+                            g.DrawPath(pen, p);
+                            g.FillPath(b, p);
+                        }
+                        return newBmp;
+                    }
+                }
             }
         }
 
+        public static Size RescaleCubic(Size size, int? height = null, int? width = null)
+        {
+            var heightScale = height.HasValue ?  size.Height - height.Value : 0;
+            var widhtScale = width.HasValue ? size.Width - width.Value : 0;
+
+            if (heightScale == 0 && widhtScale != 0)
+                heightScale = widhtScale;
+
+            if (widhtScale == 0 && heightScale != 0)
+                widhtScale = heightScale;
+
+            return new Size(size.Width - widhtScale, size.Height - heightScale);
+        }
     }
 }
